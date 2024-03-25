038bf3a944a73b00fe1f7c6b03bcb83dd025c579
sp <47121122+seongpil0948@users.noreply.github.com>
2024-03-26 01:53:01 +0900

    fuck

--- app/[lang]/home/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="relative flex flex-col h-screen">{children}</div>;
-}
--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -8,6 +8,7 @@ import { GithubIcon } from "@/components/server-only/icons";
 import config, { TAvailLocale } from "@/config";
 import { getDictionary } from "../dictionaries";
 import CommonNavbar from "@/components/server-only/navbar";
+import { GameButton } from "@/components/client-only/three-d/game-button";
 
 export async function generateStaticParams() {
   return config.i18n.locales.map((lang) => ({ lang }));
@@ -20,68 +21,14 @@ interface Param {
 export default async function SSGPage({ params: { lang } }: Param) {
   const dict = await getDictionary(lang);
   return (
-    <>
-      <CommonNavbar
-        drawerProps={{
-          title: "Home",
-        }}
-        leftTreeOptions={{
-          dir: "app/[lang]",
-        }}
-      />
-      <main>
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-          <div className="inline-block max-w-lg text-center justify-center">
-            <h1 className={title()}>{dict["home"]["teamName"]}</h1>
-            <h1 className={title({ color: "violet" })}>
-              {dict["home"]["description"]["title"]}&nbsp;
-            </h1>
-            <br />
-            <h1 className={title()}>
-              websites regardless of your design experience.
-            </h1>
-            <h2 className={subtitle({ class: "mt-4" })}>
-              Beautiful, fast and modern React UI library.
-            </h2>
-            <p className="mb-12 mt-6 max-w-6xl text-xl text-default-500">
-              {dict["home"]["description"]["summary1"]}
-              <br />
-              {dict["home"]["description"]["summary2"]}
-              <br />
-              {dict["home"]["description"]["summary3"]}
-            </p>
-          </div>
-
-          <div className="flex gap-3 flex-wrap">
-            {siteConfig.links.map((link) => {
-              return (
-                <Link
-                  key={link.href}
-                  isExternal={link.external}
-                  href={link.href}
-                  className={buttonStyles({
-                    color: "primary",
-                    radius: "full",
-                    variant: "shadow",
-                  })}
-                >
-                  {link.label.en === "GitHub" && <GithubIcon />}
-                  {link.icon}
-                  {link.label.ko}
-                </Link>
-              );
-            })}
-          </div>
-
-          <div className="mt-8">
-            <Snippet hideSymbol hideCopyButton variant="flat">
-              <span>
-                Get started by editing <Code color="primary">app/page.tsx</Code>
-              </span>
-            </Snippet>
-          </div>
-        </section>
-      </main>
-    </>
+    <div className=" m-0 p-0 w-screen h-screen overflow-hidden">
+      <h1 className={title({ color: "violet" })}>
+        {dict.home.description.title}
+      </h1>
+      <GameButton text="Game" />
+      <GameButton text="About" />
+      <GameButton text="Knowledge" />
+      <GameButton text="game" />
+    </div>
   );
 }
