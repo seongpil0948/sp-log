ebd7a3d050206b5ab8c58d6ab7e594d5f3a19041
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-04-04 09:03:28 +0900

    doc: for circle

--- app/lab/Lab.tsx
+++ app/lab/Lab.tsx
@@ -3,13 +3,15 @@
 import "./style.css";
 import clsx from "clsx";
 import { title } from "@/components/server-only/primitives";
+import CirclePoint from "./circles/circle-point";
+import CircleLine from "./circles/circle-line";
 
 export default function Lab() {
   return (
     <>
       <div
         className={clsx(
-          "w-screen h-screen text-center flex flex-col justify-between"
+          "w-screen h-screen text-center flex flex-col justify-between overflow-auto"
         )}
       >
         <div
@@ -19,6 +21,16 @@ export default function Lab() {
           })}
         >
           Lab
+          <div className=" flex flex-col gap-3 justify-center">
+            <CirclePoint
+              numPoints={200}
+              width={100}
+              height={100}
+              radius={50}
+              endAngle={Math.PI * 2}
+            />
+            <CircleLine width={100} height={100} radius={50} />
+          </div>
         </div>
       </div>
     </>
---
+++ app/lab/circles/circle-point.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+export default function CirclePoint(props: {
+  width: number;
+  height: number;
+  radius: number;
+  numPoints: number;
+  startAngle?: number; // radian
+  endAngle?: number; // radian
+}) {
+  const {
+    width,
+    height,
+    radius,
+    numPoints,
+    startAngle = 0,
+    endAngle = Math.PI / 4,
+  } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    // 원의 중심 좌표
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    // 호의 시작 각도와 종료 각도
+    // const startAngle = Math.PI / 4;
+
+    // 각 점의 좌표를 계산하고 연결하여 호를 만듭니다.
+    for (let i = 0; i < numPoints; i++) {
+      const theta =
+        startAngle + ((endAngle - startAngle) * i) / (numPoints - 1);
+      const x = centerX + radius * Math.cos(theta);
+      const y = centerY + radius * Math.sin(theta);
+
+      if (i === 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
+    }
+    ctx.stroke();
+
+    // second circle
+    ctx.translate(centerX, centerY);
+    ctx.beginPath();
+    // 원의 중심에서 반지름만큼 이동한 점에서 시작
+    ctx.moveTo(radius / 4, 0);
+    // 반시계 방향으로 원을 그림
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = "blue";
+    ctx.fill();
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width}
+      height={height}
+      style={{
+        backgroundColor: "red",
+      }}
+    />
+  );
+}
