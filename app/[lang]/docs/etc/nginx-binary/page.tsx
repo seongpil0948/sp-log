5f7325d780629f5e1b14a8c3de393dd1f2f76728
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-04-16 18:27:38 +0900

    feat(doc): in nginx-binary

---
+++ app/[lang]/docs/etc/nginx-binary/page.tsx
@@ -0,0 +1,30 @@
+import Content from "./content.mdx";
+
+import commonConfig from "@/config";
+export async function generateStaticParams() {
+  return commonConfig.i18n.locales.map((lang) => ({ lang }));
+}
+
+import { TAvailLocale } from "@/config";
+import { Metadata, ResolvingMetadata } from "next/types";
+interface Param {
+  params: { lang: TAvailLocale };
+}
+
+export async function generateMetadata(
+  { params: { lang } }: Param,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: "Nginx binary 설치",
+    description: "Nginx 설치의 정석.",
+  };
+}
+
+export default async function SSGPage({ params: { lang } }: Param) {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+}
--- components/server-only/CodeHeader.tsx
+++ components/server-only/CodeHeader.tsx
@@ -1,11 +1,21 @@
+import clsx from "clsx";
+import { paragraph, typo } from "./primitives";
+
 interface CodeHeaderProps {
   text: string;
 }
 
 export default function CodeHeader({ text }: CodeHeaderProps) {
   return (
-    <div className="rounded-t-md bg-zinc-200 px-4 py-2 font-mono text-sm text-neutral-700 dark:bg-zinc-700 dark:text-neutral-300">
-      {text}
+    <div className="rounded-t-md bg-zinc-200 text-neutral-700 dark:bg-zinc-700 dark:text-neutral-300 mt-2 md:mt-4">
+      <div
+        className={clsx(
+          typo({ font: "script", size: "lg" }),
+          "pl-2 md:pl-6 !my-0"
+        )}
+      >
+        {text}
+      </div>
     </div>
   );
 }
