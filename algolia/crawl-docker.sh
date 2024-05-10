32d838c5a706f96599af9b532e19fb4009408eaa
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-05-10 14:19:20 +0900

    hmm..

--- algolia/crawl-docker.sh
+++ algolia/crawl-docker.sh
@@ -2,4 +2,4 @@
 PATH_ENV=.env.local
 PATH_CONFIG=algolia/config.json
 
-docker run -it --env-file=$PATH_ENV -e "CONFIG=$(cat $PATH_CONFIG | jq -r tostring)" algolia/docsearch-scraper
+docker run -it --rm --env-file=$PATH_ENV -e "CONFIG=$(cat $PATH_CONFIG | jq -r tostring)" algolia/docsearch-scraper
--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,28 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: [
+          '/home',
+          '/login',
+          '/signup',
+          '/about',
+          '/contact',
+          '/terms',
+          '/privacy',
+          '/sitemap.xml',
+          '/robots.txt',
+        ],
+        disallow: '/',
+      },
+      {
+        userAgent: 'User-agent: Algolia Crawler',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: siteConfig.sitemap,
   }
 }
